438f9b3e6fcb96d862f512727bdbf6d5b73a8522
Siv Midtun Hollup <siv.hollup@gmail.com>
2014-11-17 21:37:01 +0100

    Multiple squares

--- game.js
+++ game.js
@@ -1,9 +1,9 @@
 console.log("script loaded");
 var context;
-var square = null;
+var squares = [];
 
 
-var drawCanvas = function() {
+var drawCanvas = function () {
     var canvas = document.getElementById("gameCanvas");
     canvas.height = 400;
     canvas.width = 400;
@@ -11,31 +11,34 @@ var drawCanvas = function() {
     context = canvas.getContext("2d");
 };
 
+
 var makeSquare = function (event) {
     square = {
-        x : event.pageX,
-        y : event.pageY,
-        goingRight : 1,
-        goingDown : 1
+        x: event.pageX,
+        y: event.pageY,
+        goingRight: 1,
+        goingDown: 1
     };
+    squares.push(square);
 };
 
-var draw = function (){
-    window.requestAnimationFrame(draw);
-
-    if(square === null){
+var draw = function () {
+    if (squares.size === 0) {
         return;
     }
+    window.requestAnimationFrame(draw);
+    redraw();
+};
 
+var redraw = function () {
     context.clearRect(0, 0, 400, 400);
-    if (square.goingRight === 1 && rightEdgeIsReached(square)) {
-        square.goingRight = -1;
-    }
-    square = computeMove(square);
-    context.beginPath();
-    context.rect(square.x, square.y, 15, 15);
-    context.stroke();
-    context.closePath();
+    squares.forEach(function (square) {
+        square = computeMove(square);
+        context.beginPath();
+        context.rect(square.x, square.y, 15, 15);
+        context.stroke();
+        context.closePath();
+    })
 };
 
 var computeMove = function (square) {
