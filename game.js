96651e7bfc11e641ffcbd54d77e767448000462e
Siv Midtun Hollup <siv.hollup@gmail.com>
2014-11-17 20:57:08 +0100

    Square bounces back when hitting edges

--- game.js
+++ game.js
@@ -12,10 +12,15 @@ var drawCanvas = function() {
 };
 
 var makeSquare = function (event) {
-    square = {x : event.pageX, y : event.pageY};
+    square = {
+        x : event.pageX,
+        y : event.pageY,
+        goingRight : 1,
+        goingDown : 1
+    };
 };
 
-function draw(){
+var draw = function (){
     window.requestAnimationFrame(draw);
 
     if(square === null){
@@ -23,13 +28,58 @@ function draw(){
     }
 
     context.clearRect(0, 0, 400, 400);
-    context.beginPath()
+    if (square.goingRight === 1 && rightEdgeIsReached(square)) {
+        square.goingRight = -1;
+    }
+    square = computeMove(square);
+    context.beginPath();
     context.rect(square.x, square.y, 15, 15);
     context.stroke();
     context.closePath();
+};
 
-    square.x += 2;
-    square.y += 2;
+var computeMove = function (square) {
+    adjustHorisontalDirection(square);
+    adjustVerticalDirection(square);
+    square.x += 2 * square.goingRight;
+    square.y += 2 * square.goingDown;
+    return square;
+};
 
-}
+var adjustHorisontalDirection = function (square) {
+    if (square.goingRight === 1 && rightEdgeIsReached(square)) {
+        square.goingRight = -1;
+    }
+
+    if (square.goingRight === -1 && leftEdgeIsReached(square)) {
+        square.goingRight = 1;
+    }
+};
+
+var adjustVerticalDirection = function (square) {
+    if (square.goingDown === 1 && bottomEdgeIsReached(square)) {
+        square.goingDown = -1;
+    }
+
+    if (square.goingDown === -1 && topEdgeIsReached(square)) {
+        square.goingDown = 1;
+    }
+};
+
+var rightEdgeIsReached = function (square) {
+    return (square.x >= 385);
+};
+
+var leftEdgeIsReached = function (square) {
+    return (square.x <= 1);
+};
+
+var bottomEdgeIsReached = function (square) {
+    return (square.y >= 385);
+};
+
+var topEdgeIsReached = function (square) {
+    return (square.y <= 1);
+};
+
 draw();
