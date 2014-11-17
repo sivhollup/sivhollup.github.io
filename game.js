4c9e583066067b8f306e3f55a2a0866e678c2714
Siv Midtun Hollup <siv.hollup@gmail.com>
2014-11-17 20:18:19 +0100

    Square appears and starts moving

--- game.js
+++ game.js
@@ -1,16 +1,35 @@
 console.log("script loaded");
 var context;
+var square = null;
+
 
 var drawCanvas = function() {
     var canvas = document.getElementById("gameCanvas");
-    canvas.addEventListener('click', drawSquare, false);
+    canvas.height = 400;
+    canvas.width = 400;
+    canvas.addEventListener('click', makeSquare, false);
     context = canvas.getContext("2d");
 };
 
-var drawSquare = function(event) {
-    var x = event.pageX;
-    var y = event.pageY;
-    console.log(x + " " + y);
-    context.rect(x, y, 15, 15);
-    context.stroke();
+var makeSquare = function (event) {
+    square = {x : event.pageX, y : event.pageY};
 };
+
+function draw(){
+    window.requestAnimationFrame(draw);
+
+    if(square === null){
+        return;
+    }
+
+    context.clearRect(0, 0, 400, 400);
+    context.beginPath()
+    context.rect(square.x, square.y, 15, 15);
+    context.stroke();
+    context.closePath();
+
+    square.x += 2;
+    square.y += 2;
+
+}
+draw();
